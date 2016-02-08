04a798a704a33dd44e1b680f1d6241e9b6648517
lyushninaelena <lyushnina@gmail.com>
2016-02-08 21:32:41 +0300

    Реализована функция sort
    Реализована функция sort с использованием сортировки выбором

--- sort.h
+++ sort.h
@@ -2,4 +2,16 @@
 
 template<typename T>
 void sort(std::vector<T> &arr) {
+  	for (int i = 0; i < arr.size() - 1; i++){
+		int position_min = i;
+		for (int j = i + 1; j < arr.size(); j++){
+			if (arr[j] < arr[position_min]) {
+				position_min = j;
+			}
+		}
+		T temp;
+		temp = arr[i];
+		arr[i] = arr[position_min];
+		arr[position_min] = temp;
+	}
 }
