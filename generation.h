f43dbc6a2b08bef86975505a63353a7ef9c02c18
Olga Sholokhova <sholokhova.olya@gmail.com>
2016-02-12 08:47:12 +0300

    types fix and generation func fix

--- generation.h
+++ generation.h
@@ -1,26 +1,31 @@
 #include <vector>
 #include <cstdlib>
+#include <algorithm>
 
 // repetitions_percent [0, 100] - процент повторений, 0 - все элементы различны
 template<typename T>
-void generate(std::vector<T> &arr, unsigned long long len, T min_val, T max_val, int repetitions_percent = 0) {
+void generate(std::vector<T> &arr, size_t len, T min_val, T max_val, int repetitions_percent = 0) {
 	arr.clear();
 	arr.resize(len);
-	for (int i = 1; i < len; i++) //генерация чисел на отрезке [0; (100 - repetitions_percent) / 100.0 * len]
+
+	const auto mapping_factor = (max_val - min_val) / ((100 - repetitions_percent) / 100.0 * len);
+
+	for (size_t i = 1; i < len; i++) {
 		arr[i] = (arr[i - 1] + (std::rand() % 100 + 1 > repetitions_percent ? 1 : 0));
-		arr[i - 1] = arr[i - 1] * ((max_val - min_val) / ((100 - repetitions_percent) / 100.0 * len)) + min_val; // отображение на отрезок 
+		arr[i - 1] = arr[i - 1] * mapping_factor + min_val; // отображение на отрезок
 	}
-	arr[len - 1] = arr[len - 1] * ((max_val - min_val) / ((100 - repetitions_percent) / 100.0 * len)) + min_val;
+	arr[len - 1] = arr[len - 1] * mapping_factor + min_val;
 }
 
 // order_percent [0, 100] - степень упорядоченности, 0 - массив упорядочен по возрастанию
 template<typename T>
 void shuffle(std::vector<T> &arr, int order_percent) {
+	std::random_shuffle (arr.begin(), arr.end());
 }
 
 // Генерация случайной последовательности
 template<typename T>
-void random_generate(std::vector<T> &arr, unsigned long long len, T min_val, T max_val, int repetitions_percent = 0, int order_percent = 100) {
+void random_generate(std::vector<T> &arr, size_t len, T min_val, T max_val, int repetitions_percent = 0, int order_percent = 100) {
 
 	// Генерируем упорядоченную последовательность на отрезке [min_val, max_val] с процентом повторений repetitions_percent
 	generate<T>(arr, len, min_val, max_val, repetitions_percent);
@@ -31,7 +36,7 @@ void random_generate(std::vector<T> &arr, unsigned long long len, T min_val, T m
 
 // Генерация упорядоченной последовательности
 template<typename T>
-void regular_generate(std::vector<T> &arr, unsigned long long len, T min_val, T max_val, int repetitions_percent = 0, bool sort_ascending = true) {
+void regular_generate(std::vector<T> &arr, size_t len, T min_val, T max_val, int repetitions_percent = 0, bool sort_ascending = true) {
 	// Генерируем упорядоченную последовательность на отрезке [min_val, max_val] с процентом повторений repetitions_percent
 	generate<T>(arr, len, min_val, max_val, repetitions_percent);
 
@@ -40,12 +45,12 @@ void regular_generate(std::vector<T> &arr, unsigned long long len, T min_val, T 
 
 // Генерация последовательности (чередование регулярных участков)
 template<typename T>
-void alternation_generate(std::vector<T> &arr, unsigned long long len, T min_val, T max_val) {
+void alternation_generate(std::vector<T> &arr, size_t len, T min_val, T max_val) {
 }
 
 // Генерация последовательности (смесь регулярных участков)
 template<typename T>
-void mixture_generate(std::vector<T> &arr, unsigned long long len, T min_val, T max_val) {
+void mixture_generate(std::vector<T> &arr, size_t len, T min_val, T max_val) {
 }
 
 
