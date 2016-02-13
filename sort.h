44ab8882015e7f875a85728363147441b32e6567
Axiino <max_rymar@mail.ru>
2016-02-13 23:23:26 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,11 +2,12 @@
 #include <algorithm>
 
 template<typename T>
-void sort(std::vector<T> &arr ) {
+void sort(std::vector<T> &arr, (bool) compare(T a, T b) ) {
 	for (size_t i = 0; i < arr.size() - 1; i++){
 		size_t position_min = i;
 		for (size_t j = i + 1; j < arr.size(); j++){
-			if (arr[j] < arr[position_min]) {
+			if (compare(arr[j], arr[position_min]) == true  ) {
+			//if (arr[j] < arr[position_min]) {
 				position_min = j;
 			}
 		}
