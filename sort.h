a5fa9db2575798b16b595a316391399fbb720fe7
Axiino <max_rymar@mail.ru>
2016-02-13 23:16:14 +0300

    Update sort.h

--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #include <algorithm>
 
 template<typename T>
-void sort(std::vector<T> &arr) {
+void sort(std::vector<T> &arr, ) {
 	for (size_t i = 0; i < arr.size() - 1; i++){
 		size_t position_min = i;
 		for (size_t j = i + 1; j < arr.size(); j++){
@@ -12,4 +12,11 @@ void sort(std::vector<T> &arr) {
 		}
 		std::swap(arr[i], arr[position_min]);
 	}
+}
+
+template<typename T>
+bool compare(T a, T b)
+{
+	if (a< b) return 1;
+	else return 0;
 }
