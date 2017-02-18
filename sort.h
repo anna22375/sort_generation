346c1a066af61093ee47fd49293b8a91483191c2
borisovskii <vender98@mail.ru>
2017-02-18 22:59:33 +0300

    Быстрая сортировка

--- sort.h
+++ sort.h
@@ -1,7 +1,43 @@
 #include <vector>
 #include <algorithm>
 
-template<typename T>
-void sort(std::vector<T> &arr) {
-    std::sort(arr.begin(), arr.end());
+template <class T>
+void sort(std::vector<T> &array, const int le, const int ri)
+{
+        if (le >= ri) return;
+        int j = partition(array, le, ri);
+        sort(array, le, j - 1);
+        sort(array, j + 1, ri);
+}
+
+template <class T>
+void sort(std::vector<T> &array)
+{
+        static int le = 0;
+        static int ri = array.size()-1;
+        if (le >= ri) return;
+        int j = partition(array, le, ri);
+        sort(array, le, j - 1);
+        sort(array, j + 1, ri);
+}
+
+
+template <class T>
+int partition(std::vector<T> &array, const int le, const int ri)
+{
+        
+        int i = le + 1;
+        int j = ri;
+        while (i <= j)
+        {
+                if (array[i] < array[le]) i++;
+                else if (array[j] > array[le]) j--;
+                else {
+                        swap(array[i], array[j]);
+                        i++;
+                        j--;
+                }
+        }
+        swap(array[j], array[le]);
+        return j;
 }
