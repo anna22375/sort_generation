7e5674326c388e9f0ab1233feb28de10e599ffce
panchenko-rv <panchenko-rv@yandex.ru>
2017-02-19 15:13:00 +0300

    QuickSort

--- sort.h
+++ sort.h
@@ -1,7 +1,31 @@
 #include <vector>
 #include <algorithm>
 
+template <typename T>
+void swap(T &a, T &b) {
+    T t = a;
+      a = b;
+      b = t;
+}
+
+template <typename T>
+void quicksort(std::vector<T> &arr, int l, int r) {
+    int p = l-1;
+    if (l < r) {
+        T pivot = arr[r];
+        for (int i = l; i < r; i++)
+            if (arr[i] <= pivot) {
+                p++;
+                swap(arr[p], arr[i]);
+            }
+        p++;
+        swap(arr[p], arr[r]);
+        quicksort(arr, l, p-1);
+        quicksort(arr, p+1, r);
+    }
+}
+
 template<typename T>
 void sort(std::vector<T> &arr) {
-    std::sort(arr.begin(), arr.end());
+    quicksort(arr, 0, arr.size()-1);
 }
