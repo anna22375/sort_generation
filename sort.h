4936bc4f41c1355f0f9c8722ec54c4d36dd1e1cc
borisovskii <vender98@mail.ru>
2017-03-05 00:42:56 +0300

    add intro sort

--- sort.h
+++ sort.h
@@ -1,43 +1,83 @@
 #include <vector>
 #include <algorithm>
 
-template <class T>
-void sort(std::vector<T> &array, const int le, const int ri)
+template <typename T>
+int partition(std::vector<T> &a, const int l, const int r)
 {
-        if (le >= ri) return;
-        int j = partition(array, le, ri);
-        sort(array, le, j - 1);
-        sort(array, j + 1, ri);
+        int j = (l + r) / 2;
+        if (a[j] > a[l])
+                if (a[j] > a[r])
+                        if (a[l] > a[r])
+                                std::swap(a[l], a[l]);
+                        else std::swap(a[l], a[r]);
+                else std::swap(a[l], a[j]);
+        else
+                if (a[j] < a[r])
+                        if (a[l] < a[r])
+                                std::swap(a[l], a[l]);
+                        else std::swap(a[l], a[r]);
+                else std::swap(a[l], a[j]);
+
+                j = l;
+                for (int i = l + 1; i <= r; i++)
+                        if (a[i] < a[l])
+                        {
+                                j++; std::swap(a[j], a[i]);
+                        }
+                std::swap(a[l], a[j]);
+                return j;
 }
 
-template <class T>
-void sort(std::vector<T> &array)
+template <typename T>
+void sift(std::vector<T> &a, int l, int r)
 {
-        static int le = 0;
-        static int ri = array.size()-1;
-        if (le >= ri) return;
-        int j = partition(array, le, ri);
-        sort(array, le, j - 1);
-        sort(array, j + 1, ri);
-}
-
-
-template <class T>
-int partition(std::vector<T> &array, const int le, const int ri)
-{
-        
-        int i = le + 1;
-        int j = ri;
-        while (i <= j)
+        int i = l;
+        int j = 2 * i;
+        T y = a[i];
+        while (j <= r) //i - номер текущего места y
+                                   //j - номер левого "опорного" элемента
         {
-                if (array[i] < array[le]) i++;
-                else if (array[j] > array[le]) j--;
-                else {
-                        swap(array[i], array[j]);
-                        i++;
-                        j--;
-                }
+                if (j < r)			//оба "опорных" входят в массив
+                        if (a[j] < a[j + 1])
+                                j++;	//выбрали max из "опорных"
+                if (y >= a[j]) break;		//встали на место
+                                                                        //идем "вниз":
+                a[i] = a[j];
+                i = j;
+                j = 2 * i;
         }
-        swap(array[j], array[le]);
-        return j;
+        a[i] = y;
+}
+
+template <typename T>
+void heapSort(std::vector<T> &a)
+{
+        int n = a.size() - 1;
+        // n - номер последнего элемента (массив [1..n])
+        // фаза 1: формирование пирамиды
+        for (int i = (n / 2); i > 0; i--)
+                sift(a, i, n);
+        // фаза 2: полное упорядочение
+        for (int i = n; i > 1; i--)
+        {
+                swap(a[1], a[i]);
+                sift(a, 1, i - 1);
+        }
+}
+
+template <typename T>
+void sort(std::vector<T> &a, const int l, const int r)
+{
+        if (l >= r) return;
+        int j = partition(a, l, r);
+
+        if ((j - l) > 16)
+                sort(a, l, j - 1);
+        else
+                heapSort(a);
+
+        if ((r - j) > 16)
+                sort(a, j + 1, r);
+        else
+                heapSort(a);
 }
