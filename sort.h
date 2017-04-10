1210fa316a34e3dedc2da162b12f4dc3dc6b1e30
natasha liss <natasha liss>
2017-04-10 10:25:18 +0300

    quick sort

--- sort.h
+++ sort.h
@@ -1,7 +1,48 @@
 #include <vector>
 #include <algorithm>
 
+void swap(int* a, int* b)
+{
+	int t = *a;
+	*a = *b;
+	*b = t;
+}
+
+int partition(int arr[], int l, int r, int& swapping)
+{
+	int pivot = arr[r];
+	int i = (l - 1);  // Index of smaller element
+
+	for (int j = l; j <= r - 1; j++)
+	{
+
+		if (arr[j] <= pivot)
+		{
+			i++;
+			swap(&arr[i], &arr[j]);
+			swapping++;
+		}
+	}
+	swap(&arr[i + 1], &arr[r]);
+	swapping++;
+	return (i + 1);//return the meaning of pivot
+}
+
+void quickSort(int arr[], int l, int r, int& swapping)
+{
+
+	if (l < r)
+	{
+		int pi = partition(arr, l, r, swapping);//pi is partitioning index
+
+		quickSort(arr, l, pi - 1, swapping);
+		quickSort(arr, pi + 1, r, swapping);
+	}
+
+}
+
 template<typename T>
 void sort(std::vector<T> &arr) {
-    std::stable_sort(arr.begin(), arr.end());
+	int swapping;
+	quickSort(arr.data(), 0, arr.size() - 1, swapping);
 }
